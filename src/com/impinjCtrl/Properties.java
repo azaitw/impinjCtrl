f2b666efe111bffb6421f6b4f0f1a088c5b61dde
Kai-Sheng Chan <azai@Azai-MBP.local>
2017-05-09 23:14:07 +0800

    Comments

--- src/com/impinjCtrl/Properties.java
+++ src/com/impinjCtrl/Properties.java
@@ -1,8 +1,5 @@
 package com.impinjCtrl;
 
-/**
- * Created by azai on 05/09/2017.
- */
 public class Properties {
     public static String hostname = "hostname";
     public static String powerDbm = "powerDbm";
