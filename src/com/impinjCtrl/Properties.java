ce79d57df9de4acee7e82a15c611e8ee2b6fd01d
jimhcc <jimhcc@yhoo-inc.com>
2017-05-29 16:22:09 +0800

    logs && socket.io

--- src/com/impinjCtrl/Properties.java
+++
@@ -1,8 +0,0 @@
-package com.impinjCtrl;
-
-public class Properties {
-    public static String hostname = "hostname";
-    public static String powerDbm = "powerDbm";
-    public static String sensitivityDbm = "sensitivityDbm";
-    public static String debugMode = "debugMode";
-}
