7116c2d68bb639e9abfa18b3b949a036d34add32
Azai Chan <azai.tw@gmail.com>
2017-11-13 14:06:14 +0800

    Get property name directly

--- src/main/java/lib/PropertyUtils.java
+++ src/main/java/lib/PropertyUtils.java
@@ -1,14 +1,13 @@
 package lib;
 
-import com.impinjCtrl.Properties;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PropertyUtils {
     public static String getLogFileName() { return new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date()) + ".json"; }
-    public static String getAPiHost() { return System.getProperty(Properties.apiHost); }
-    public static String getReaderHost() { return System.getProperty(Properties.readerHost); }
-    public static String getLogPath() { return System.getProperty(Properties.logPath, "./log/"); }
+    public static String getAPiHost() { return System.getProperty("apiHost"); }
+    public static String getReaderHost() { return System.getProperty("readerHost"); }
+    public static String getLogPath() { return System.getProperty("logPath", "./log/"); }
     public static Long getTimestamp() {
         return System.currentTimeMillis();
     }
